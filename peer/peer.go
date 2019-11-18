4287d94966abaf159808d983bcb21554421b55e3
Henry Syahputra <syahputra.hsp@gmail.com>
2019-11-18 17:13:37 +0800

    added peer with multihash multiaddrs

--- peer/peer.go
+++ peer/peer.go
@@ -1,1 +1,33 @@
 package peer
+
+import (
+	mh "github.com/multiformats/go-multihash"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+type Peer struct {
+	Id mh.Multihash
+	Addresses []*ma.Multiaddr
+}
+
+func (p *Peer) AddAddress(a *ma.Multiaddr){
+	p.Addresses = append(p.Addresses, a)
+}
+
+func (p *Peer) NetAddress(n string) *ma.Multiaddr{
+	for _, a := range p.Addresses{
+		ps, err = a.Protocols()
+		if err != nil {
+			continue // address is invalid
+		}
+	}
+
+	for _, p := range ps {
+		if p.Name == n {
+			return a
+		}
+	}
+
+	return nil
+}
+
