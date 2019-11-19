0ba9c92dfc0e488cdcaff7db307ac0e66d4ecf4c
Henry Syahputra <syahputra.hsp@gmail.com>
2019-11-19 13:51:53 +0800

    util

---
+++ util/util.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"fmt"
+	"os"
+)
+
+var Debug bool
+var NotImplementedError = fmt.Errorf("Error: not implemented yet")
+
+func PErr(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+func POut(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stdout, format, a...)
+}
+
+func DErr(format string, a ...interface{}) {
+	if Debug {
+		PErr(format, a...)
+	}
+}
+
+func DOut(format string, a ...interface{}) {
+	if Debug {
+		POut(format, a...)
+	}
+}
